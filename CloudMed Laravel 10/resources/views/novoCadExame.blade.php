afa79496444fc3261e58cf0a7c7d3257d1966fe3
Major2571 <caroline.tacats62@gmail.com>
2023-05-01 18:42:31 -0300

    style: css and filters input

--- CloudMed Laravel 10/resources/views/novoCadExame.blade.php
+++ CloudMed Laravel 10/resources/views/novoCadExame.blade.php
@@ -103,11 +103,11 @@
                 </div>
 
                 <div class="button">
-                    <input class="btnSalvar" type="submit" value="Salvar">
+                    <button type="button" class="text-cyan-700 hover:text-white border border-cyan-700 hover:bg-cyan-800 focus:ring-2 focus:outline-none focus:ring-cyan-50 font-medium rounded-lg px-7 py-2 text-center mr-2 mb-2">Salvar</button>
                 </div>
             </form>
         </div>
 
     </section>
-    
-@endsection
+
+    @endsection
