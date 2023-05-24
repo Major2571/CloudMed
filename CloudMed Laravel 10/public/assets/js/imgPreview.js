347bad19fbcf41ab3832d1030e17ea2fd0fe216d
Major2571 <caroline.tacats62@gmail.com>
2023-05-23 22:00:30 -0300

    Add pdf preview

--- CloudMed Laravel 10/public/assets/js/imgPreview.js
+++ CloudMed Laravel 10/public/assets/js/imgPreview.js
@@ -7,11 +7,32 @@ let fileInfo = document.getElementById('file-info');
 uploadButton.onchange = () => {
     let file = uploadButton.files[0];
     if (file) {
-        if (file.type.includes('image/')) {
+
+
+        if (file.type === 'application/pdf') {
+            let newEmbed = document.createElement('embed');
+            newEmbed.setAttribute('src', URL.createObjectURL(file));
+            newEmbed.setAttribute('type', file.type);
+            newEmbed.setAttribute('width', '100%');
+            newEmbed.setAttribute('height', '600px');
+            previewContainer.innerHTML = '';
+            previewContainer.appendChild(newEmbed);
+
+            fileName.textContent = file.name;
+            previewContainer.style.display = 'block';
+
+            fileInfo.textContent = '';
+            fileInfo.style.display = 'none';
+        }
+
+        else if (file.type.includes('image/')) {
             let reader = new FileReader();
             reader.readAsDataURL(file);
             reader.onload = () => {
-                chosenImage.setAttribute('src', reader.result);
+                let newImage = document.createElement('img');
+                newImage.setAttribute('src', reader.result);
+                previewContainer.innerHTML = '';
+                previewContainer.appendChild(newImage);
             }
             fileName.textContent = file.name;
             previewContainer.style.display = 'block';
@@ -19,12 +40,15 @@ uploadButton.onchange = () => {
             fileInfo.textContent = '';
             fileInfo.style.display = 'none';
 
-        } else {
-            previewContainer.style.display = 'none'; 
+        }
+
+        else {
+            previewContainer.style.display = 'none';
             fileInfo.textContent = 'Arquivo selecionado: ' + file.name;
             fileInfo.style.display = 'block';
         }
-        
+
+
     } else {
         previewContainer.style.display = 'none';
         fileInfo.textContent = '';
