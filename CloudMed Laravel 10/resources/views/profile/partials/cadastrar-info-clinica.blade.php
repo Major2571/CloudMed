6b025b2a6e6196b0c55e4febd17a483919705ded
Major2571 <caroline.tacats62@gmail.com>
2023-06-01 17:37:09 -0300

    Added JS regex

--- CloudMed Laravel 10/resources/views/profile/partials/cadastrar-info-clinica.blade.php
+++ CloudMed Laravel 10/resources/views/profile/partials/cadastrar-info-clinica.blade.php
@@ -1,78 +1,80 @@
-<div class="profile-forms">
-    <form method="POST" action="{{ route('infoClinica.store') }}">
-        @csrf
-        <div class="title-profile mb-5">
-            <h2> Informações Clínicas </h2>
+<form method="POST" action="{{ route('infoClinica.store') }}">
+    @csrf
+    <div class="title-profile mb-5">
+        <h2> Informações Clínicas </h2>
+    </div>
+
+    <div class="grid gap-x-5 md:grid-cols-2">
+
+        <div class="mb-3">
+            <div class="relative w-full">
+                <label for="tipoSanguineo" class="mb-2 block font-medium text-gray-900"> Tipo
+                    Sanguínio
+                </label>
+                <select name="tipoSanguineo"
+                    class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 pr-10 text-gray-900 focus:border-cyan-700 focus:ring-cyan-700">
+                    <option value="">Selecione</option>
+                    <option value="O+">O+</option>
+                    <option value="O-">O-</option>
+                    <option value="A+">A+</option>
+                    <option value="A-">A-</option>
+                    <option value="B+">B+</option>
+                    <option value="B-">B-</option>
+                    <option value="AB+">AB+</option>
+                    <option value="AB-">AB-</option>
+                </select>
+            </div>
         </div>
-    
-        <div class="grid md:grid-cols-2 gap-x-5">
-    
-            <div class="mb-3">
-                <div class="relative w-full">
-                    <label for="tipoSanguineo" class="mb-2 block font-medium text-gray-900"> Tipo
-                        Sanguínio
-                    </label>
-                    <select name="tipoSanguineo"
-                        class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 pr-10 text-gray-900 focus:border-cyan-700 focus:ring-cyan-700">
-                        <option value="">Selecione</option>
-                        <option value="O+">O+</option>
-                        <option value="O-">O-</option>
-                        <option value="A+">A+</option>
-                        <option value="A-">A-</option>
-                        <option value="B+">B+</option>
-                        <option value="B-">B-</option>
-                        <option value="AB+">AB+</option>
-                        <option value="AB-">AB-</option>
-                    </select>
-                </div>
+
+        <div class="mb-3">
+            <div class="relative w-full">
+                <label for="doador" class="mb-2 block font-medium text-gray-900"> É doador de
+                    orgãos?
+                </label>
+                <select name="doador"
+                    class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 pr-10 text-gray-900 focus:border-cyan-700 focus:ring-cyan-700">
+                    <option value="">Selecione</option>
+                    <option value="Sim">Sim</option>
+                    <option value="Não">Não</option>
+                </select>
             </div>
-    
-            <div class="mb-3">
-                <div class="relative w-full">
-                    <label for="doador" class="mb-2 block font-medium text-gray-900"> É doador de
-                        orgãos?
-                    </label>
-                    <select name="doador"
-                        class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 pr-10 text-gray-900 focus:border-cyan-700 focus:ring-cyan-700">
-                        <option value="">Selecione</option>
-                        <option value="Sim">Sim</option>
-                        <option value="Não">Não</option>
-                    </select>
-                </div>
-            </div>
-    
-            <div class="mb-3">
-                <div class="relative">
-                    <x-input-label for="altura" :value="__('Altura:')" />
-                    <div class="flex items-center">
-                        <x-text-input type="text" name="altura" placeholder="Ex: 170" id="altura" required />
-                        <span class="ml-2 pb-3"> cm </span>
-                    </div>
-                </div>
-            </div>
-    
-            <div class="mb-3">
-                <div class="relative w-full">
-                    <x-input-label for="peso" :value="__('Peso:')" />
-                    <div class="flex items-center">
-                        <x-text-input type="text" name="peso" placeholder="Ex: 65,4" id="peso" required />
-                        <span class="ml-2 pb-3"> kg </span>
-                    </div>
-                </div>
-            </div>
-    
-            <div class="mb-3 col-span-2">
-                <div class="relative w-full">
-                    <x-input-label for="alergias" :value="__('Alergias:')" />
-                    <textarea id="alergias" name="alergias" rows="4" class="p-2.5 w-full bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-cyan-700 focus:border-cyan-700 block" placeholder="Escreva suas principais alergias..."></textarea>
-                </div>
-            </div>
-    
         </div>
-        <div class="btn-profile">
-            <button type="submit" onclick="saveProfileInfo()"
-                class="mb-2 rounded-lg border border-emerald-600 px-7 py-2 text-center font-medium text-emerald-800 hover:bg-emerald-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-emerald-50"
-                value="Salvar">Salvar</button>
+
+        <div class="mb-3">
+            <div class="relative">
+                <x-input-label for="altura" :value="__('Altura:')" />
+                <div class="flex items-center">
+                    <x-text-input type="text" name="altura" placeholder="Ex: 170" id="altura" max="300"
+                        onkeyup="formatarAltura(this)" required />
+                    <span class="ml-2 pb-3"> cm </span>
+                </div>
+            </div>
         </div>
-    </form>
-</div>
+
+        <div class="mb-3">
+            <div class="relative w-full">
+                <x-input-label for="peso" :value="__('Peso:')" />
+                <div class="flex items-center">
+                    <x-text-input type="text" name="peso" placeholder="Ex: 65,4" id="peso" max="300"
+                        onkeyup="formatarPeso(this)" required />
+                    <span class="ml-2 pb-3"> kg </span>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-span-2 mb-3">
+            <div class="relative w-full">
+                <x-input-label for="alergias" :value="__('Alergias:')" />
+                <textarea id="alergias" name="alergias" rows="4"
+                    class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-cyan-700 focus:ring-cyan-700"
+                    placeholder="Escreva suas principais alergias..."></textarea>
+            </div>
+        </div>
+
+    </div>
+    <div class="btn-profile">
+        <button type="submit" onclick="saveProfileInfo()"
+            class="mb-2 rounded-lg border border-emerald-600 px-7 py-2 text-center font-medium text-emerald-800 hover:bg-emerald-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-emerald-50"
+            value="Salvar">Salvar</button>
+    </div>
+</form>
--- CloudMed Laravel 10/resources/views/profile/partials/update-password-form.blade.php
+++ CloudMed Laravel 10/resources/views/profile/partials/update-password-form.blade.php
@@ -1,54 +1,45 @@
 @section('title', 'Alterar Senha')
-<x-app-layout>
-    <section class="login session-allow min-h-screen">
 
-        <section class="profile my-5 w-full flex justify-center items-center ">
-            <div class="filter drop-shadow-md md:w-4/5 flex justify-center">
-                <x-sidebar />
+@extends('layouts.profile')
 
-                <div class="profile-forms">
+@section('conteudo')
+    <form method="post" action="{{ route('password.update') }}" class="mt-6 space-y-6">
+        @csrf
+        @method('put')
 
-                    <form method="post" action="{{ route('password.update') }}" class="mt-6 space-y-6">
-                        @csrf
-                        @method('put')
+        <div class="title-profile mb-5">
+            <h2> Alterar senha atual </h2>
+        </div>
 
-                        <div class="title-profile mb-5">
-                            <h2> Alterar senha atual </h2>
-                        </div>
+        <div>
+            <x-input-label for="current_password" :value="__('Senha atual:')" />
+            <x-text-input id="current_password" name="current_password" type="password" class="mt-1 block w-full"
+                autocomplete="current-password" />
+            <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
+        </div>
 
-                        <div>
-                            <x-input-label for="current_password" :value="__('Senha atual:')" />
-                            <x-text-input id="current_password" name="current_password" type="password"
-                                class="mt-1 block w-full" autocomplete="current-password" />
-                            <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
-                        </div>
+        <div>
+            <x-input-label for="password" :value="__('Nova senha')" />
+            <x-text-input id="password" name="password" type="password" class="mt-1 block w-full"
+                autocomplete="new-password" />
+            <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
+        </div>
 
-                        <div>
-                            <x-input-label for="password" :value="__('Nova senha')" />
-                            <x-text-input id="password" name="password" type="password" class="mt-1 block w-full"
-                                autocomplete="new-password" />
-                            <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
-                        </div>
+        <div>
+            <x-input-label for="password_confirmation" :value="__('Confirme sua senha:')" />
+            <x-text-input id="password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full"
+                autocomplete="new-password" />
+            <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
+        </div>
 
-                        <div>
-                            <x-input-label for="password_confirmation" :value="__('Confirme sua senha:')" />
-                            <x-text-input id="password_confirmation" name="password_confirmation" type="password"
-                                class="mt-1 block w-full" autocomplete="new-password" />
-                            <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
-                        </div>
+        <div class="btn-profile">
+            <x-primary-button>{{ __('Salvar') }}</x-primary-button>
 
-                        <div class="btn-profile">
-                            <x-primary-button>{{ __('Salvar') }}</x-primary-button>
-
-                            @if (session('status') === 'password-updated')
-                                <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)"
-                                    class="text-sm text-gray-600 dark:text-gray-400">
-                                    {{ __('Senha alterada com sucesso!') }}</p>
-                            @endif
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </section>
-    </section>
-</x-app-layout>
+            @if (session('status') === 'password-updated')
+                <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)"
+                    class="text-sm text-gray-600 dark:text-gray-400">
+                    {{ __('Senha alterada com sucesso!') }}</p>
+            @endif
+        </div>
+    </form>
+@endsection
