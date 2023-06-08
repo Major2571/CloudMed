bc82b1a537f307f2f51d6e97514f1713cc67e824
Major2571 <caroline.tacats62@gmail.com>
2023-06-08 17:20:20 -0300

    Add icon

--- CloudMed Laravel 10/resources/views/templates/headLogin.blade.php
+++ CloudMed Laravel 10/resources/views/templates/headLogin.blade.php
@@ -16,6 +16,18 @@
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"
+        integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"
+        integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/datepicker.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.15.5/dist/sweetalert2.all.min.js"></script>
+
     <link rel="shortcut icon" href="{{url('assets/img/icon-logo.png')}}" type="image/x-icon">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -23,10 +35,11 @@
     @include('templates.styles')
 
 <body>
-    <button onclick="goBack()" class="goBack">
-        <img src="https://blog.social.mg.gov.br/wp-content/plugins/photo-gallery/css/bwg-fonts/fonts/bwg-fonts-svg/angle-up-sm.svg" alt="">
-        <p> Voltar </p>
-    </button>
+    <div class="absolute w-24 my-5 mx-10">
+        <a href="/">
+            <img src="{{ url('assets/img/logo.png') }}" alt="logo">
+        </a>
+    </div>
     
     @yield('contentLogin')
 
