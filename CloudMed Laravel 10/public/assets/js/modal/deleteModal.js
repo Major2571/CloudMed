69a851ab18b123e952d1c44458ad5e2fdbeb2dbb
Major2571 <caroline.tacats62@gmail.com>
2023-06-14 22:24:15 -0300

    Change btns color

--- CloudMed Laravel 10/public/assets/js/modal/deleteModal.js
+++ CloudMed Laravel 10/public/assets/js/modal/deleteModal.js
@@ -5,8 +5,8 @@ function confirmExclusao(event) {
         text: 'Essa ação não pode ser revertida!',
         icon: 'warning',
         showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#DE3F4D',
+        confirmButtonColor: '#DE3F4D',
+        cancelButtonColor: '#3085d6',
         confirmButtonText: 'Sim, excluir!'
     }).then((result) => {
         if (result.isConfirmed) {
